de225e16b8192156b27453a08a6fc51a04deda6d
dev-nich-m2 <csapuras@gmail.com>
2026-02-12 11:31:39 +0800

    add: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./templates/*.html"],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
